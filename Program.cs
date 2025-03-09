5aa3ba3c2ebe18bf8541ade4a16f8c6be4ddebcc
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2025-03-08 22:10:35 -0300

    feat: initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+using var driver = new ChromeDriver();
+
+string baseUrl = "http://the-internet.herokuapp.com/";
+driver.Url = baseUrl;
+
+void automateCheckboxes()
+{
+    var checkboxLink = driver.FindElement(By.XPath("/html/body/div[2]/div/ul/li[6]/a"));
+    checkboxLink.Click();
+
+    var upperCheckbox = driver.FindElement(By.XPath("//*[@id=\"checkboxes\"]/input[1]"));
+    var lowerCheckbox = driver.FindElement(By.XPath("//*[@id=\"checkboxes\"]/input[2]"));
+
+    for (int i = 0; i < 100; i++)
+    {
+        upperCheckbox.Click();
+        lowerCheckbox.Click();
+    }
+
+    driver.Navigate().GoToUrl(baseUrl);
+}
+
+void automateContextMenu()
+{
+    var contextMenuLink = driver.FindElement(By.XPath("/html/body/div[2]/div/ul/li[7]/a"));
+    contextMenuLink.Click();
+}
+
+
+
+automateCheckboxes();
+
+Console.WriteLine("Press enter to exit. . .");
+Console.ReadLine();
